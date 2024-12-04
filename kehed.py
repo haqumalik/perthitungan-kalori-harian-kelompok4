1fa69093789eea8944ddb1f76df76028b1ad9d23
Nathania <elekotek06@gmail.com>
2024-12-04 12:49:18 +0700

    commit dani

--- kehed.py
+++ kehed.py
@@ -92,6 +92,7 @@ class SistemKalori:
         elif jenis_kelamin == "P":
             return 2000 - (8 * (usia // 10))
         return 2000
+
     
     def input_makanan(self):
         self.load_makanan()
